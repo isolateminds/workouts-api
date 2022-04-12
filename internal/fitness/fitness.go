e2b0c244b8c15667345fa5625170ea77d7bad8d3
dannyvidal <danny@dannyvidal.com>
2022-04-11 23:15:33 -0400

    fitness package

---
+++ internal/fitness/fitness.go
@@ -0,0 +1,47 @@
+package fitness
+
+import (
+	"encoding/csv"
+	"os"
+)
+
+type FitnessExercise struct {
+	BodyPart  string `json:"bodypart"`
+	Equipment string `json:"equipment"`
+	GifURL    string `json:"gifurl"`
+	Id        string `json:"id"`
+	Name      string `json:"name"`
+	Target    string `json:"target"`
+}
+
+func ParseFE(path string) []FitnessExercise {
+
+	file, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+
+	csvReader := csv.NewReader(file)
+
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	fitnessExercises := make([]FitnessExercise, len(records)-1)
+	for idx, record := range records {
+		// 0th index is csv headers
+		if idx != 0 {
+			fitnessExercises[idx-1] = FitnessExercise{
+				record[0],
+				record[1],
+				record[2],
+				record[3],
+				record[4],
+				record[5],
+			}
+		}
+
+	}
+	return fitnessExercises
+}
